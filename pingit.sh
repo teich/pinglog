a121587c445a3fd0bb019de81e688aee078eb53e
Oren Teich <oren@teich.net>
2015-08-25 14:54:17 -0700

    shhh, be quiet'

--- pingit.sh
+++ pingit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LOG_URL="http://localhost:4000/api/logs"
+export LOG_URL="https://enigmatic-ocean-6979.herokuapp.com/api/logs"
 
 function ping-time {
         echo $(ping -c 1 $1 | grep 'time=' | awk '{print $7}' | cut -f 2 -d '=')
@@ -15,4 +15,4 @@ gateway="$(netstat -rn | grep 'default' | awk '{print $2}')"
 google_ping=$(ping-time 8.8.8.8)
 gateway_ping=$(ping-time $gateway)
 
-curl -X "POST" $LOG_URL -H "Content-Type: application/json" --data '{"log":{"network":"'"$ssid"'", "gateway":"'"$gateway_ping"'", "google":"'"$google_ping"'", "host":"'"$uuid"'"}}'
+curl -s -X "POST" $LOG_URL -H "Content-Type: application/json" --data '{"log":{"network":"'"$ssid"'", "gateway":"'"$gateway_ping"'", "google":"'"$google_ping"'", "host":"'"$uuid"'"}}' > /dev/null
