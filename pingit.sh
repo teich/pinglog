e08f1d2bfba7da3ca22babe0cb178a07d21b811d
Oren Teich <oren@teich.net>
2015-08-25 11:00:04 -0700

    initial commit

---
+++ pingit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SBHOME=~/src/pinglog/shellbrato
+LBUSER=oren@teich.net
+
+source .env
+source $SBHOME/shellbrato.sh
+
+function ping-time {
+        echo $(ping -c 1 $1 | grep 'time=' | awk '{print $7}' | cut -f 2 -d '=')
+}
+
+now="$(date +%s)"
+ssid="$(networksetup -getairportnetwork en0 | cut -f 4 -d ' ')"
+uuid="$(ioreg -rd1 -c IOPlatformExpertDevice | awk '/IOPlatformUUID/ { split($0, line, "\""); printf("%s\n", line[4]); }')"
+gateway="$(netstat -rn | grep 'default' | awk '{print $2}')"
+
+google_ping=$(ping-time 8.8.8.8)
+gateway_ping=$(ping-time $gateway)
+
+echo "$now, $ssid, $google_ping, $uuid, $gateway_ping"
+
+# sendGauge "${now}||google_ping||${ping}||${ssid}"
