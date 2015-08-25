6c5cfacbf6cdd4ee126dff7e254ed7ec3215e89b
Oren Teich <oren@teich.net>
2015-08-25 14:33:05 -0700

    ping script escapes JSON for post now

--- pingit.sh
+++ pingit.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#SBHOME=~/src/pinglog/shellbrato
-#LBUSER=oren@teich.net
-
-#source .env
-#source $SBHOME/shellbrato.sh
+export LOG_URL="http://localhost:4000/api/logs"
 
 function ping-time {
         echo $(ping -c 1 $1 | grep 'time=' | awk '{print $7}' | cut -f 2 -d '=')
@@ -19,6 +15,4 @@ gateway="$(netstat -rn | grep 'default' | awk '{print $2}')"
 google_ping=$(ping-time 8.8.8.8)
 gateway_ping=$(ping-time $gateway)
 
-echo "$now, $uuid, $ssid, $gateway_ping, $google_ping"
-
-# sendGauge "${now}||google_ping||${ping}||${ssid}"
+curl -X "POST" $LOG_URL -H "Content-Type: application/json" --data '{"log":{"network":"'"$ssid"'", "gateway":"'"$gateway_ping"'", "google":"'"$google_ping"'", "host":"'"$uuid"'"}}'
